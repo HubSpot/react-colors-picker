1f7bdd21fe5b4829bd60ccf660427e6c8bdcee6e
lbryant <lbryant@hubspot.com>
2016-03-14 19:49:10 -0400

    test

--- static/test/spec/colorpicker.js
+++ static/test/spec/colorpicker.js
@@ -1,5 +1,8 @@
 'use es6';
 
+import React from 'react';
+import {renderIntoDocument} from 'react.addons.TestUtils';
+import {findDOMNode} from 'react-dom';
 import ColorPicker from 'react-colors-picker/ColorPicker';
 import {getRenderOutput} from 'react-utils/testing/ShallowRenderUtils';
 
@@ -12,5 +15,13 @@ describe('Colorpicker', () => {
   it('full featured', () => {
     const picker = getRenderOutput(<ColorPicker hexOnly={false} includeAlpha={true} />);
     expect(picker.props.className).toContain('with-alpha');
+  });
+
+  it('renders favorites', () => {
+    const picker = renderIntoDocument(<ColorPicker favorites={['#333333', '#1F9900', '#71C9AD']} />);
+    const pickerEl = findDOMNode(picker);
+    const favoritesEl = pickerEl.querySelector('.favorites');
+    expect(favoritesEl).toBeDefined();
+    expect(favoritesEl.querySelectorAll('.react-colorpicker-panel-preview').length).toBe(3);
   })
 });
